47436eda8e6405f41514141166d3bead58ebce32
ST10496115 <ST10496115@vcconnect.edu.za>
2025-06-19 12:33:46 +0200

    exam

--- app/src/main/java/za/co/st10496115exam1/MainActivity.kt
+++ app/src/main/java/za/co/st10496115exam1/MainActivity.kt
@@ -1,5 +1,6 @@
 package za.co.st10496115exam1
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
@@ -10,28 +11,28 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-@Suppress("UNUSED_VARIABLE")
+
 class MainActivity : AppCompatActivity() {
+
+
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        var playlist=findViewById<TextView>(R.id.playlist)
+       var songtitle= findViewById<EditText>(R.id.songtitle)
+        var artistname=findViewById<EditText>(R.id.artistname)
 
+        var rating=findViewById<EditText>(R.id.rating)
+        var `nextbutton`=findViewById<Button>(R.id.button)
+        var enterbutton=findViewById<Button>(R.id.button2)
 
-       var playlist=findViewById<TextView>(R.id.textView2)
-        var songtitle=findViewById<TextView>(R.id.textView)
-        var artistname=findViewById<EditText>(R.id.editTextText)
-        var rating=findViewById<EditText>(R.id.editTextText2)
-        var comments=findViewById<EditText>(R.id.editTextText3)
-        var enter=findViewById<Button>(R.id.button)
-        val next=findViewById<Button>(R.id.button2)
-
-
-      next.setOnClickListener {
-          val intent=Intent(this,store::class.java)
-          startActivity(intent)
-      }
+       `nextbutton`.setOnClickListener {
+           val intent=Intent(this,store::class.java)
+           startActivity(intent)
+       }
 
 
 
@@ -43,8 +44,10 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
+
     }
+}
+
 }
 
 
